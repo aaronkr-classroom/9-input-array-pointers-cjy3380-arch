c5fe9017a588eed0bbd963212afdef295dc325ca
cjy <cjy3380@gmail.com>
2025-11-16 22:39:03 +0900

    finish

---
+++ calculator.c
@@ -0,0 +1,77 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+#define MAX_CH 100
+#define NUM_CH MAX_CH/2
+
+int numcheck(char ch) {
+	return (int)ch >= '0' && ch <= '9';
+
+}
+
+int check0p(char op) {
+	return op == '+' || op == '-' || op == '*' || op == '/';
+
+}
+
+int main(void) {
+	char input[MAX_CH],
+		numStr1[NUM_CH], numStr2[NUM_CH],
+		operator = '\0';
+	
+	int i = 0,
+		j = 0;
+	printf("Enter a simple math formula(12+34):\n");
+	fgets(input, sizeof(input), stdin);
+
+
+	while (input[i] != '\0' && numcheck(input[i])) {
+		numStr1[j++] = input[i++];
+
+	}
+	numStr1[j] = '\0';
+	if (check0p(input[i])) {
+		operator=input[i++];
+	}
+	else {
+		printf("Error:invalid operator\n");
+		return 1;
+	}
+
+	j = 0;
+	while (input[i] != '\0' && numcheck(input[i])) {
+		numStr2[j++] = input[i++];
+
+	}
+	numStr2[j] = '\0';
+
+	int num1 = atoi(numStr1);
+	int num2 = atoi(numStr2);
+	int result = 0;
+
+	switch (operator) {
+	case'+':
+		result = num1 + num2;
+		break;
+	case'-':
+		result = num1 - num2;
+		break;
+	case'*':
+		result = num1 * num2;
+		break;
+	case'/':
+		if(num2>0)result = num1 / num2;
+		else {
+			printf("error:division by 0\n");
+		}
+		break;
+	default:
+		printf("error:invalid operator. use + - * / \n");
+		return 1;
+	}
+
+	printf("result:%d %c %d = %d", num1, operator,num2, result);
+	
+	return 0;
+}
---
+++ getmystr.c
@@ -0,0 +1,43 @@
+﻿
+#include <stdio.h>
+
+#define MAX_CH 100
+
+int getmystr(char buffer[], int limit) {
+	int i = 0;
+	int ch;
+	while ((ch = getchar()) != '\n' && ch != EOF) {
+		if (i < limit - 1) {
+			buffer[i++] = (char)ch;
+		}
+		else {
+			while ((ch = getchar()) != '\n' && getchar() != EOF);
+			buffer[i] = '\0';
+			return 0;
+		}
+	}
+	for (i < limit; i++;) {
+		buffer[i] = getchar();
+		if (buffer[i] == '\n') {
+			buffer[i] = 0;
+			return 1;
+		}
+		buffer[i] = 0;
+		rewind(stdin);
+	}
+	return 0;
+	
+}
+
+int main(void) {
+	char input_str[MAX_CH];
+	int state;
+
+	printf("Enter text:");
+	state = getmystr(input_str, MAX_CH);
+
+	if (state)printf("input:%s\n", MAX_CH);
+	else printf("input:%s->out of range\n", input_str);
+
+	return 0;
+}
